e1a68dc103f5868489bfd4bf7718804871146a42
David Alyohin <dalehin277@gmail.com>
2023-10-16 23:30:28 +0900

    Updating the Calculator

--- CalculatorHard/src/Main.java
+++ CalculatorHard/src/Main.java
@@ -2,10 +2,30 @@ import java.util.Scanner;
 import static java.lang.System.*;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(in);
-        int a = sc.nextInt();
-        char op = sc.next().charAt(0);
-        int b = sc.nextInt();
+        try {
+            Scanner sc = new Scanner(in);
+
+            out.print(" __" + "|Type an Expression:\n" + "|\n" + " ==> ");
+            int a = sc.nextInt();
+            char op = sc.next().charAt(0);
+            int b = sc.nextInt();
+
+            Calc(a, op, b);
+
+            if (a > 1000) {
+                throw new Exception("Variable <'a'> can't be more than 1000!!!");
+            }
+            if (b > 1000) {
+                throw new Exception("Variable <'b'> can't be more than 1000!!!");
+            }
+
+        } catch (java.lang.ArithmeticException ex) {
+            out.println("You can't divide by zero!!!");
+        } catch (Exception ex) {
+            out.println(ex.getMessage());
+        }
+    }
+    public static void Calc(int a, char op, int b) {
         int result;
 
         switch (op) {
@@ -22,8 +42,11 @@ public class Main {
                 result = a / b;
                 break;
             default:
-                System.out.println("Введите верную операцию!");
+                out.print("Type the right operation!!!\n" + "-> : (<+> <-> <*> </>) : <-");
                 return;
+        }
+        if (a <= 1000 && b <= 1000) {
+            out.println(result);
         }
     }
 }
