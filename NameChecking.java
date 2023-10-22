06ccad3374d90133ee662c1f69e87d21242c3f06
David Alyohin <dalehin277@gmail.com>
2023-10-22 21:38:05 +0900

    New update for Namechecking.java

--- NameChecking.java
+++ NameChecking.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class NameChecking {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Input your name:\n" + "==> ");
         boolean isCorrectName = false;
         while (!isCorrectName) {
             String name = scanner.nextLine(); //Считывает строку из System.in
@@ -23,20 +24,31 @@ public class NameChecking {
 
     private static String formatName(String name) {
         String[] fullName = name.trim().split(" ");
-        sortByLength(fullName);
-        String s1 = name, s2 = "";
+        String s = "";
 
-        s2 = s2 + s1.substring(0, 1).toUpperCase();
-        for (int i = 1; i < s1.length(); i++) {
-            // смотрим, был ли слева пробел:
-            if (" ".equals(s1.substring(i - 1, i)))
-                s2 = s2 + s1.substring(i, i + 1).toUpperCase();
-            else
-                s2 = s2 + s1.substring(i, i + 1);
+        for (int i = 0; i < fullName.length; i++) {
+            fullName[i] = fullName[i].substring(0, 1).toUpperCase() + fullName[i].substring(1);
+            s = fullName[0] + " " + fullName[1] + " " + fullName[2];
         }
-        return s2;
+        sortByLength(fullName);
+        return s;
     }
 
     private static void sortByLength(String[] words) {
+        boolean isSorted = false;
+        String s = "";
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < words.length - 1; i++) {
+                if (words[i].length() > words[i + 1].length()) {
+                    isSorted = false;
+
+                    s = words[i];
+                    words[i] = words[i + 1];
+                    words[i + 1] = s;
+                }
+            }
+        }
+        System.out.println(words[0] + " " + words[1] + " " + words[2]);
     }
 }
